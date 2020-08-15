cd298ad9bd7e7b77b9c853fe3cf7bff842c269e7
k.omi <>
2020-08-15 17:54:05 +0900

    投稿詳細画面の追加とルーティング設定

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -11,9 +11,13 @@ class BoardsController < ApplicationController
         Board.create(board_params)
     end
 
+    def show
+        @board = Board.find(params[:id])
+    end
+
     private
 
     def board_params
-       params.require(:board).permit(:name, :title, :body)
+        params.require(:board).permit(:name, :title, :body)
     end
 end    
