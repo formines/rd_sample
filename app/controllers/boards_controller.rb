5b29bd5d900d25485bb596dc01b470f562bdd472
k.omi <>
2020-11-15 21:32:33 +0900

    表示件数をセレクトで切り替えるように修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,7 +3,7 @@ class BoardsController < ApplicationController
 
     def index
       @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all.order(id: "DESC") 
-      @boards = @boards.page(params[:page])
+      @boards = @boards.page(params[:page]).per(params[:count])
     end
 
     def new
